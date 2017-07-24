b3d815df7d379bbb3830513d01a20547d9c09e02
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-07-24 09:50:34 +0100

    Update

---
+++ SAUnityAdvertiserSDK/Assets/main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperAwesome;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		SACPI.getInstance ().handleInstall ((success) => {
+			Debug.Log ("Handled install " + success);
+		});
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
